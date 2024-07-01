0348b40e737e1b6bfe4112fdac39ceb54f22289d
Lumir Balhar <lbalhar@redhat.com>
2024-07-01 12:40:39 +0200

    CVE-2024-37891

--- src/urllib3/util/retry.py
+++ src/urllib3/util/retry.py
@@ -151,7 +151,7 @@ class Retry(object):
 
     RETRY_AFTER_STATUS_CODES = frozenset([413, 429, 503])
 
-    DEFAULT_REDIRECT_HEADERS_BLACKLIST = frozenset(['Cookie', 'Authorization'])
+    DEFAULT_REDIRECT_HEADERS_BLACKLIST = frozenset(['Cookie', 'Authorization', 'Proxy-Authorization'])
 
     #: Maximum backoff time.
     BACKOFF_MAX = 120
